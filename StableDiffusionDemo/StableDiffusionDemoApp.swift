e33c5edac53c962785c166838ce19fa6e4dd5948
Simon Whitaker <sw@netcetera.org>
2022-12-14 12:35:54 +0000

    Initial Commit

---
+++ StableDiffusionDemo/StableDiffusionDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StableDiffusionDemoApp.swift
+//  StableDiffusionDemo
+//
+//  Created by Simon on 14/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct StableDiffusionDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
